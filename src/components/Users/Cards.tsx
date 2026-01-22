c5aa757e424000f70d64dcf249d8d68710c7e5f8
sivasathish889 <sivasathish889@gmail.com>
2026-01-22 20:11:37 +0530

    key declartion error fixed

--- src/components/Users/Cards.tsx
+++ src/components/Users/Cards.tsx
@@ -2,7 +2,7 @@ import type { UserCardType } from "../../@types/types"
 
 const Cards = ({ id, title, value, percentage }: UserCardType) => {
   return (
-    <div className="card shadow-sm border border-gray-200 p-6 rounded-xl w-48 bg-white h-32">
+    <div className="card shadow-sm border border-gray-200 p-6 rounded-xl w-48 bg-white h-32" key={id}>
       <div className="card-header space-y-2 flex flex-col justify-center">
         <p className="text-base opacity-65">{title}</p>
         <p className="text-lg">{value}</p>
